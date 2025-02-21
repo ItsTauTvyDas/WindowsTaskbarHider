d3257d17c0555a3f50d113f9ed2ef6bfaef2de65
Tautvydas <tautvydas.gaming@gmail.com>
2025-02-21 08:53:02 +0200

    Remove predefined application name variable

--- globals.cpp
+++ globals.cpp
@@ -1,7 +1,6 @@
 #include "globals.h"
 #include "taskbar.h"
 
-LPCSTR globals::app = "Windows Taskbar Hider";
 LPCSTR globals::exe;
 HWND globals::hWnd = nullptr;
 bool globals::noMessageBoxes = false;
