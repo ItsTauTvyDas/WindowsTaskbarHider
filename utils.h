edde82dc00e4f1cc87cf295488f6b70b2dbab4f6
Tautvydas <tautvydas.gaming@gmail.com>
2025-02-22 03:17:23 +0200

    Add new methods

--- utils.h
+++ utils.h
@@ -14,17 +14,17 @@ public:
     static HICON loadExeIcon(LPCSTR pszExeFileName, UINT nIconIndex);
     static void showExceptionMessageBox(const std::function<void(std::stringstream&)>& callback);
     static LPSTR NTStatusMessageToText(DWORD NTStatusMessage);
-    static void throwIfNoDLLIcons(const std::string &dllPath);
     static std::string joinString(const std::vector<std::string> &vec, const std::string &delimiter);
     static std::vector<std::string> splitString(const std::string &str, char delimiter);
-    static void attachConsoleWindow();
     static bool fileExists(const char *path);
     static bool doesAutoStart();
     static void toggleStartup();
-    static void toggleConsoleWindow();
+    static void toggleConsoleWindow(PHANDLER_ROUTINE handler, bool status);
     static void toUnicode(LPCCH string, LPWSTR str);
+    static void clearConsole(COORD startCoord);
 private:
-    static std::string createShortcutLinkPath(char appPath[]);
+    static std::string createShortcutLinkPath();
+    static void attachConsoleWindow();
 };
 
 #endif //UTILS_H
