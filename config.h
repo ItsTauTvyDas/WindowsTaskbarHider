06158d6357d047dc1bb257387c7e19dd338621aa
Tautvydas <tautvydas.gaming@gmail.com>
2025-02-24 19:10:53 +0200

    Add alwaysIgnoreWhenNotMaximized and exceptionalWindows

--- config.h
+++ config.h
@@ -8,9 +8,11 @@ class config {
 public:
     static bool debug;
     static bool keepConsoleWindowOpen;
+    static bool alwaysIgnoreWhenNotMaximized;
     static int taskbarUpdateInterval;
     static int opacity;
     static std::vector<std::string> ignoredWindows;
+    static std::vector<std::string> exceptionalWindows;
 
     static void load();
     static void save();
