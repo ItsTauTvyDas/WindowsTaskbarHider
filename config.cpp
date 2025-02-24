4be4e067e65d7696cdfd190d0f603af2c37f0dc6
Tautvydas <tautvydas.gaming@gmail.com>
2025-02-24 19:10:16 +0200

    Add ExceptionalWindows support

--- config.cpp
+++ config.cpp
@@ -13,9 +13,11 @@
 
 bool config::debug = false;
 bool config::keepConsoleWindowOpen = false;
+bool config::alwaysIgnoreWhenNotMaximized = true;
 int config::taskbarUpdateInterval = 10;
 int config::opacity = 0;
 std::vector<std::string> config::ignoredWindows = {"title:", "process:ApplicationFrameHost.exe"};
+std::vector<std::string> config::exceptionalWindows = {};
 
 void config::save() {
     std::ofstream file(CONFIG_FILENAME, std::ios::out | std::ios::trunc);
@@ -25,15 +27,18 @@ void config::save() {
     }
     file << "[General]" << std::endl;
     file << "; If enabled (1), application will open a console window" << std::endl;
-    file << "Debug=" << debug << std::endl;
+    file << "DebugEnabled = " << debug << std::endl;
     file << "; Keep console window open if user sends CTRL+C action to console window" << std::endl;
-    file << "KeepConsoleWindowOpen=" << keepConsoleWindowOpen << std::endl;
+    file << "KeepConsoleWindowOpen = " << keepConsoleWindowOpen << std::endl;
     file << "[Taskbar]" << std::endl;
     file << "; Taskbar update loop interval in milliseconds" << std::endl;
-    file << "UpdateInterval=" << taskbarUpdateInterval << std::endl;
+    file << "UpdateInterval = " << taskbarUpdateInterval << std::endl;
     file << "; Opacity from 0 to 255" << std::endl;
-    file << "Opacity=" << opacity << std::endl;
-    file << "; Available tags: process, title, class" << std::endl;
+    file << "Opacity = " << opacity << std::endl;
+    file << "[Ignored Windows]" << std::endl;
+    file << "; Setting this to false (0) could slow down the application with debug mode on" << std::endl;
+    file << "AlwaysIgnoreWhenNotMaximized = " << alwaysIgnoreWhenNotMaximized << std::endl;
+    file << "; Available tags: process/p (text), title/t (text), class/c (text), focus/f (0 or 1), maximized/m (0 or 1), left (int), top (int), right (int), bottom (int)" << std::endl;
     file << "; Separator: |" << std::endl;
     file << ";" << std::endl;
     file << "; Ignore UWP container window and windows with empty titles" << std::endl;
@@ -41,7 +46,8 @@ void config::save() {
     file << "; Some of the processes seems to have maximized windows, even though they are not visible" << std::endl;
     file << "; We don't have a way to distinguish between that invisible window," << std::endl;
     file << "; so the taskbar is going to be still invisible when opening something like Settings" << std::endl;
-    file << "IgnoreMaximizedWindows=" << utils::joinString(ignoredWindows, "|") << std::endl;
+    file << "IgnoredWindows = " << utils::joinString(ignoredWindows, "|") << std::endl;
+    file << "ExceptionalWindows = " << std::endl;
     file.flush();
     file.close();
 }
@@ -79,16 +85,21 @@ bool config::processSingle(const std::string &key, const std::string &value) {
         if (key == "Taskbar.UpdateInterval") {
             taskbarUpdateInterval = std::stoi(value);
             invalidIntegerValue(formattedKey, taskbarUpdateInterval, 1, 1000);
-        } else if (key == "General.Debug") {
+        } else if (key == "General.DebugEnabled") {
             debug = value != "0";
         } else if (key == "General.KeepConsoleWindowOpen") {
             keepConsoleWindowOpen = value != "0";
         } else if (key == "Taskbar.Opacity") {
             opacity = std::stoi(value);
             invalidIntegerValue(formattedKey, opacity, 0, 255);
-        } else if (key == "Taskbar.IgnoreMaximizedWindows") {
+        } else if (key == "Ignored_Windows.IgnoredWindows") {
             ignoredWindows = utils::splitString(value, '|');
+        } else if (key == "Ignored_Windows.ExceptionalWindows") {
+            exceptionalWindows = utils::splitString(value, '|');
+        } else if (key == "Ignored_Windows.AlwaysIgnoreWhenNotMaximized") {
+            alwaysIgnoreWhenNotMaximized = value != "0";
         } else {
+            MessageBoxA(globals::hWnd, ("Invalid configuration key: " + key).c_str(), PROJECT_NAME, MB_ICONWARNING | MB_OK);
             return false;
         }
     } catch (const std::exception& e) {
@@ -123,8 +134,11 @@ void config::load() {
         if (pos == std::string::npos)
             continue;
 
-        const std::string key = prefix + line.substr(0, pos);
-        const std::string value = line.substr(pos + 1);
+        std::string key = line.substr(0, pos);
+        utils::trim(key);
+        key = prefix + key;
+        std::string value = line.substr(pos + 1);
+        utils::trim(value);
 
         processSingle(key, value);
     }
--- taskbar.cpp
+++ taskbar.cpp
@@ -1,10 +1,17 @@
 #include "taskbar.h"
 
 #include <iostream>
+#include <cmath>
 #include "config.h"
 #include "utils.h"
+#include <iomanip>
+
+int debug_column_sizes[] = {1, 1, 1, 10, 10, 2, 1, 4, 4, 4, 4, 20, 20, 20};
+constexpr int debug_columns_total_width = 102 + 2 * sizeof(debug_column_sizes);
 
 bool taskbar::wasDebugFlushed = false;
+bool taskbar::canUpdateDebugMessages = false;
+int consoleMaxLength = 0;
 
 HWND taskbar::getTaskbarHandle() {
     return FindWindowW(L"Shell_TrayWnd", nullptr);
@@ -23,56 +30,224 @@ bool taskbar::isCursorOverTaskbar() {
     return PtInRect(&taskbarRect, cursorPos);
 }
 
+bool loopThroughWindowTags(std::vector<std::string> vector, const HWND hwnd, std::string &processName, std::string &title,
+    std::string &wndclass, DWORD &focusStatus, WINDOWINFO &wi, const WINDOWPLACEMENT &wp, RECT wRect, bool &priority,
+    std::string &succeededTagGroup) {
+    for (const auto& tagGroup: vector) {
+        if (tagGroup.length() == 0)
+            continue;
+        bool _priority = false;
+        std::vector<std::string> tags = utils::splitString(tagGroup, '&');
+        if (tags.size() == 0)
+            continue;
+        if (tags[0].rfind("^", 0) == 0) {
+            tags[0] = tags[0].substr(1);
+            _priority = true;
+        }
+        int succeededTags = 0;
+        for (const auto& texpr : tags) {
+            const auto pos = texpr.find(":", 0);
+            if (pos == std::string::npos)
+                continue;
+            char _[256];
+            std::string key = texpr.substr(0, pos);
+            std::string value = texpr.substr(pos + 1);
+            if (key == "process" || key == "p") {
+                if (processName.length() == 0)
+                    utils::getProcessInfo(hwnd, processName);
+                if (processName == value)
+                    succeededTags++;
+            } else if (key == "title" || key == "t") {
+                if (title.length() == 0) {
+                    GetWindowTextA(hwnd, _, sizeof(_));
+                    title = _;
+                }
+                if (title == value)
+                    succeededTags++;
+            } else if (key == "focus" || key == "f") {
+                if (focusStatus == -1) {
+                    wi.cbSize = sizeof(WINDOWINFO);
+                    GetWindowInfo(hwnd, &wi);
+                    focusStatus = wi.dwWindowStatus;
+                }
+                if (focusStatus == stoi(value))
+                    succeededTags++;
+            } else if (key == "class" || key == "c") {
+                if (wndclass.length() == 0) {
+                    GetClassNameA(hwnd, _, sizeof(_));
+                    wndclass = _;
+                }
+                if (std::string(wndclass) == value)
+                    succeededTags++;
+            } else if (key == "maximized" || key == "m") {
+                if (wp.showCmd == SW_MAXIMIZE == stoi(value))
+                    succeededTags++;
+            } else if (key == "left" || key == "right" || key == "top" || key == "bottom") {
+                if (wRect.left == -1 && wRect.top == -1 && wRect.right == -1 && wRect.bottom == -1)
+                    GetWindowRect(hwnd, &wRect);
+                const int iValue = std::stoi(value);
+                int rect;
+                if (key == "left")
+                    rect = wRect.left;
+                else if (key == "right")
+                    rect = wRect.right;
+                else if (key == "top")
+                    rect = wRect.top;
+                else if (key == "bottom")
+                    rect = wRect.bottom;
+                else
+                    continue;
+                if (rect == iValue)
+                    succeededTags++;
+            }
+        }
+        if (succeededTags == tags.size()) {
+            succeededTagGroup = tagGroup;
+            priority = _priority;
+            return true;
+        }
+    }
+    return false;
+}
+
+void stdCOutRepeat(const char ch, const int len, const bool end) {
+    if (!len) return;
+    for (auto i = 0; i < len; i++)
+        std::cout << ch;
+    if (end)
+        std::cout << std::endl;
+}
+
 bool taskbar::isAnyWindowMaximized() {
     bool maximized = false;
     wasDebugFlushed = false;
-    if (config::debug) {
+
+    if (config::debug && canUpdateDebugMessages) {
         utils::clearConsole({ 0, 2 });
         std::cout << "[DEBUG] Loop start" << std::endl;
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        if(GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {
+            consoleMaxLength = csbi.srWindow.Right-csbi.srWindow.Left;
+            if (consoleMaxLength < debug_columns_total_width) {
+                std::cout << "[DEBUG] Table couldn't be formatted, required min width " << debug_columns_total_width << ", got " << consoleMaxLength << std::endl;
+                consoleMaxLength = 0;
+            }
+            stdCOutRepeat('#', consoleMaxLength, true);
+        }
     }
-    EnumWindows([](HWND hwnd, const LPARAM lParam) -> BOOL {
-        if (GetWindow(hwnd, GW_OWNER) != nullptr) return TRUE;
-        if (!IsWindowVisible(hwnd)) return TRUE;
 
+    EnumWindows([](HWND hwnd, const LPARAM lParam) -> BOOL {
         WINDOWPLACEMENT wp;
         wp.length = sizeof(WINDOWPLACEMENT);
-        if (!GetWindowPlacement(hwnd, &wp) || wp.showCmd != SW_MAXIMIZE)
+
+        if (!GetWindowPlacement(hwnd, &wp) || !IsWindowVisible(hwnd) || IsIconic(hwnd))
             return TRUE;
-        const std::string processName = utils::getProcessName(hwnd);
 
-        char title[256];
-        GetWindowTextA(hwnd, title, sizeof(title));
-        char wndclass[256];
-        GetClassNameA(hwnd, wndclass, sizeof(wndclass));
+        if (config::alwaysIgnoreWhenNotMaximized && wp.showCmd != SW_MAXIMIZE)
+            return TRUE;
 
-        if (config::debug)
-            std::cout << "[DEBUG] Found maximized window:" << std::endl
-                      << "[DEBUG]     Title    = " << processName << std::endl
-                      << "[DEBUG]     Process  = " << processName << std::endl
-                      << "[DEBUG]     Class    = " << processName << std::endl
-                      << "[DEBUG]     Skipped? = ";
+        RECT wRect { -1, -1, -1, -1 };
+        std::string processName = "",
+                    title = "",
+                    wndclass = "",
+                    succeededIgnoreTagGroup = "",
+                    succeededExceptionTagGroup = "";
+        WINDOWINFO wi;
+        DWORD focusStatus = -1;
+
+        if (config::debug && canUpdateDebugMessages) {
+            // Focus status (0 or 1)
+            wi.cbSize = sizeof(WINDOWINFO);
+            GetWindowInfo(hwnd, &wi);
+            focusStatus = wi.dwWindowStatus;
+            // Process filename
+            utils::getProcessInfo(hwnd, processName);
+            // Title
+            char _[256];
+            GetWindowTextA(hwnd, _, sizeof(_));
+            title = _;
+            // Class
+            GetClassNameA(hwnd, _, sizeof(_));
+            wndclass = _;
+            // Rect
+            GetWindowRect(hwnd, &wRect);
+        }
+
+        bool ignored = true, exceptional = false, priority = false;
+
         if (!config::ignoredWindows.empty()) {
-            bool skipped = false;
-            for (const auto& window: config::ignoredWindows) {
-                if ("process:" + processName == window)
-                    skipped = true;
-                if ("title:" + std::string(title) == window)
-                    skipped = true;
-                if ("class:" + std::string(wndclass) == window)
-                    skipped = true;
-                if (skipped) {
-                    if (config::debug)
-                        std::cout << "Yes" << std::endl;
-                    return TRUE;
+            bool _;
+            ignored = loopThroughWindowTags(config::ignoredWindows, hwnd, processName, title, wndclass, focusStatus, wi, wp, wRect, _, succeededIgnoreTagGroup);
+        }
+
+        if (!config::exceptionalWindows.empty()) {
+            exceptional = loopThroughWindowTags(config::exceptionalWindows, hwnd, processName, title, wndclass, focusStatus, wi, wp, wRect, priority, succeededExceptionTagGroup);
+        }
+
+        if (config::debug && canUpdateDebugMessages) {
+            if (!consoleMaxLength) {
+                std::cout << " ig=" << ignored
+                          << " ex=" << exceptional
+                          << " pr=" << priority
+                          << " igT=" << (succeededIgnoreTagGroup.length() == 0 ? "NUL" : succeededIgnoreTagGroup)
+                          << " exT=" << (succeededExceptionTagGroup.length() == 0 ? "NUL" : succeededExceptionTagGroup)
+                          << " f=" << focusStatus
+                          << " m=" << (wp.showCmd == SW_MAXIMIZE)
+                          << " sl=" << wRect.left
+                          << " st=" << wRect.top
+                          << " sr=" << wRect.right
+                          << " sb=" << wRect.bottom
+                          << " p=" << processName
+                          << " c=" << wndclass
+                          << " t=" << title << std::endl;
+            } else {
+                try {
+                    std::vector<std::string> values = {};
+                    values.push_back(std::to_string(ignored));
+                    values.push_back(std::to_string(exceptional));
+                    values.push_back(std::to_string(priority));
+                    values.push_back(succeededIgnoreTagGroup.length() == 0 ? "NUL" : succeededIgnoreTagGroup);
+                    values.push_back(succeededExceptionTagGroup.length() == 0 ? "NUL" : succeededExceptionTagGroup);
+                    values.push_back(std::to_string(focusStatus));
+                    values.push_back(std::to_string(wp.showCmd == SW_MAXIMIZE));
+                    values.push_back(std::to_string(wRect.left));
+                    values.push_back(std::to_string(wRect.top));
+                    values.push_back(std::to_string(wRect.right));
+                    values.push_back(std::to_string(wRect.bottom));
+                    values.push_back(processName);
+                    values.push_back(wndclass);
+                    values.push_back(title);
+                    std::cout << '#';
+                    for (auto i = 0; i < values.size(); i++) {
+                        std::cout << ' ';
+                        const int size = debug_column_sizes[i];
+                        if (size < values[i].length())
+                            debug_column_sizes[i] = values[i].length();
+                        std::cout << std::setw(size) << std::left << values[i];
+                        std::cout << ' ';
+                        if (i < values.size() - 1)
+                            std::cout << '#';
+                    }
+                    std::cout << '#' << std::endl;
+                } catch (const std::exception e) {
+                    std::cout << "ERROR: " << e.what() << std::endl;
                 }
             }
         }
-        if (config::debug)
-            std::cout << "No" << std::endl;
-        *reinterpret_cast<bool*>(lParam) = true;
-        return FALSE;
+
+        if (exceptional || !ignored || priority) {
+            *reinterpret_cast<bool*>(lParam) = true;
+            return FALSE;
+        }
+        return TRUE;
     }, reinterpret_cast<LPARAM>(&maximized));
+    if (config::debug && canUpdateDebugMessages) {
+        stdCOutRepeat('#', consoleMaxLength, true);
+        if (!maximized)
+            std::cout << "[DEBUG] No maximized windows found." << std::endl;
+    }
     wasDebugFlushed = true;
+    canUpdateDebugMessages = false;
     return maximized;
 }
 
