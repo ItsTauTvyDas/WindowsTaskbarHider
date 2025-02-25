3d4e8141d9f8bc1c101f6655995b894532a1906b
Tautvydas <tautvydas.gaming@gmail.com>
2025-02-25 22:36:24 +0200

    Add default (english) language files

---
+++ language.h
@@ -0,0 +1,52 @@
+#ifndef LANGUAGE_H
+#define LANGUAGE_H
+
+#define MSG_APPLICATION_NAME                  1
+
+#define MSG_OUTPUT_STREAM_FAILED              2
+
+#define MSG_SHORTCUT_CREATION_FAILED          3
+#define MSG_SHORTCUT_ADD_VERIFY               4
+#define MSG_SHORTCUT_REMOVE_VERIFY            5
+
+#define MSG_TASKBAR_VISIBILITY_FIXED          6
+
+#define MSG_CONFIG_LOAD_FAILED                7
+#define MSG_CONFIG_INVALID_NUMBER             8
+#define MSG_CONFIG_BELOW_MIN                  9
+#define MSG_CONFIG_ABOVE_MAX                  10
+#define MSG_CONFIG_INVALID_KEY                11
+#define MSG_CONFIG_NOT_NUMBER                 12
+
+#define MSG_ARGS_INVALID                      13
+
+#define MSG_UNCAUGHT_EXCEPTION_HEADER         14
+#define MSG_UNCAUGHT_EXCEPTION_RETRY          15
+#define MSG_UNCAUGHT_EXCEPTION_RETRY_FAILED   16
+#define MSG_UNCAUGHT_EXCEPTION_WILL_TERMINATE 17
+#define MSG_UNCAUGHT_EXCEPTION_TRANSLATED_MSG 18
+#define MSG_UNCAUGHT_EXCEPTION_INFO           19
+#define MSG_UNCAUGHT_EXCEPTION_INFO_CODE      20
+#define MSG_UNCAUGHT_EXCEPTION_INFO_ADDR      21
+
+#define MSG_WINDOW_CREATION_FAILED            22
+#define MSG_WINDOW_REGISTER_FAILED            23
+#define MSG_WINDOW_IRON_FAILED                24
+
+#define MSG_MUTEX_FAILED                      25
+
+#define MSG_APP_ALREADY_RUNNING_BUT_KILLED    26
+#define MSG_APP_ALREADY_RUNNING               27
+
+#define MSG_APP_WIN_DARK_MODE                 28
+
+#define MSG_TRAY_EXIT                         29
+#define MSG_TRAY_OPEN_GITHUB                  30
+#define MSG_TRAY_ADD_STARTUP                  31
+#define MSG_TRAY_REM_STARTUP                  32
+#define MSG_TRAY_TB_PAUSE                     33
+#define MSG_TRAY_TB_RESUME                    34
+#define MSG_TRAY_CONFIG_RELOAD                35
+#define MSG_TRAY_CONFIG_OPEN                  36
+
+#endif //LANGUAGE_H
