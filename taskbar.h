ae8e9a90dd29133917648dfb78b90597b9a3ba72
Tautvydas <tautvydas.gaming@gmail.com>
2025-02-27 06:13:18 +0200

    Remove == operator function and add finalDetection variable to WindowInfo struct

--- taskbar.h
+++ taskbar.h
@@ -13,28 +13,13 @@ public:
         bool detected = false;
         bool wasExceptional = false;
         bool wasRectModified = false;
+        bool finalDetection = false;
         DWORD focused = -1;
         wchar_t wndClass[256] = {};
         RECT rect;
         wchar_t title[256] = {};
         std::wstring fault;
         std::wstring procFilename;
-
-        bool operator==(const WindowInfo& o) const {
-            return maximized       == o.maximized &&
-                   detected        == o.detected &&
-                   wasExceptional  == o.wasExceptional &&
-                   wasRectModified == o.wasRectModified &&
-                   focused           == o.focused &&
-                   wndClass        == o.wndClass &&
-                   rect.left       == o.rect.left &&
-                   rect.top        == o.rect.top &&
-                   rect.right      == o.rect.right &&
-                   rect.bottom     == o.rect.bottom &&
-                   title           == o.title &&
-                   fault           == o.fault &&
-                   procFilename    == o.procFilename;
-        }
     };
 
     static std::vector<WindowInfo> windows;
