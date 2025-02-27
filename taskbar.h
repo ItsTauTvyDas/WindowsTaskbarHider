2e826a7e7189b8ee249569485529848195c494b6
Tautvydas <tautvydas.gaming@gmail.com>
2025-02-27 03:15:59 +0200

    Add WindowInfo struct

--- taskbar.h
+++ taskbar.h
@@ -2,9 +2,44 @@
 #define TASKBAR_H
 
 #include <dwmapi.h>
+#include <string>
+#include <vector>
 
 class taskbar {
 public:
+    struct WindowInfo {
+        HWND hwnd = nullptr;
+        bool maximized = false;
+        bool detected = false;
+        bool wasExceptional = false;
+        bool wasRectModified = false;
+        int focus = -1;
+        wchar_t wndClass[256] = {};
+        RECT rect;
+        wchar_t title[256] = {};
+        std::wstring fault;
+        std::wstring procFilename;
+
+        bool operator==(const WindowInfo& o) const {
+            return maximized       == o.maximized &&
+                   detected        == o.detected &&
+                   wasExceptional  == o.wasExceptional &&
+                   wasRectModified == o.wasRectModified &&
+                   focus           == o.focus &&
+                   wndClass        == o.wndClass &&
+                   rect.left       == o.rect.left &&
+                   rect.top        == o.rect.top &&
+                   rect.right      == o.rect.right &&
+                   rect.bottom     == o.rect.bottom &&
+                   title           == o.title &&
+                   fault           == o.fault &&
+                   procFilename    == o.procFilename;
+        }
+    };
+
+    static std::vector<WindowInfo> windows;
+    static bool collectWindowsInfo;
+
     static HWND getTaskbarHandle();
     static void setTaskbarVisibility(bool visible);
     static void resetTaskbar();
