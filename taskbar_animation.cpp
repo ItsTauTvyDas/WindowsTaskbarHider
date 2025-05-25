ced5b27f2973f658790d940f221eb63bea2222fa
Tautvydas <tautvydas.gaming@gmail.com>
2025-05-25 05:15:34 +0300

    Optimize code and fix some bugs

--- taskbar_animation.cpp
+++ taskbar_animation.cpp
@@ -7,47 +7,39 @@
 
 #include "config.h"
 #include "globals.h"
+#include "utils.h"
 
-std::unordered_map<HWND, int> taskbar_animation::animatingState = {};
+std::unordered_map<HWND, int> animatingState = {};
 std::thread taskbar_animation::animationThread;
-std::mutex animationMutex;
+std::mutex taskbar_animation::animationMutex;
+
+inline int nextOpacity(HWND taskbar, const int visibleOpacityPoint) {
+    BYTE opacityBytes = 0;
+    if (!GetLayeredWindowAttributes(taskbar, nullptr, &opacityBytes, nullptr))
+        return 255;
+    const int currentOpacity = opacityBytes;
+    const int delta = (utils::mouseInWindow(taskbar) ? 1 : -1) * config::animationOpacityStep;
+    return std::clamp(currentOpacity + delta, visibleOpacityPoint, config::opacityWhenHoveredInternal.load());
+}
 
 void animationLoop() {
     while (true) {
-        {
-            std::lock_guard lock(animationMutex);
-
-            for (auto it = taskbar_animation::animatingState.begin(); it != taskbar_animation::animatingState.end();) {
-                HWND taskbar = it->first;
-                const int visibleOpacityPoint = it->second;
-
-                POINT cursorPos;
-                RECT taskbarRect;
-
-                GetCursorPos(&cursorPos);
-                GetWindowRect(taskbar, &taskbarRect);
-
-                BYTE opacityBytes = 0;
-                if (GetLayeredWindowAttributes(taskbar, nullptr, &opacityBytes, nullptr)) {
-                    int currentOpacity = opacityBytes;
-
-                    const int delta = (PtInRect(&taskbarRect, cursorPos) ? 1 : -1) * config::animationOpacityStep;
-                    currentOpacity = std::clamp(currentOpacity + delta, visibleOpacityPoint, config::opacityWhenHoveredInternal);
-
-                    if (currentOpacity <= visibleOpacityPoint || currentOpacity >= config::opacityWhenHoveredInternal) {
-                        it = taskbar_animation::animatingState.erase(it);
-                        continue;
-                    }
-
-                    SetLayeredWindowAttributes(taskbar, 0, static_cast<BYTE>(currentOpacity), LWA_ALPHA);
-                }
-                ++it;
-            }
-        }
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(config::animationStepDelay));
         if (globals::isShuttingDown)
             break;
+        std::vector<std::pair<HWND, int>> states;
+        {
+            std::lock_guard lock(taskbar_animation::animationMutex);
+            states.assign(animatingState.begin(), animatingState.end());
+        }
+        for (auto [taskbar, visibleOpacityPoint] : states) {
+            const int currentOpacity = nextOpacity(taskbar, visibleOpacityPoint);
+            SetLayeredWindowAttributes(taskbar, 0, static_cast<BYTE>(currentOpacity), LWA_ALPHA);
+            if (currentOpacity == visibleOpacityPoint || currentOpacity == config::opacityWhenHoveredInternal) {
+                std::lock_guard lock(taskbar_animation::animationMutex);
+                animatingState.erase(taskbar);
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(config::animationStepDelay));
     }
 }
 
@@ -58,18 +50,18 @@ void taskbar_animation::initThread() {
 }
 
 bool taskbar_animation::isAnimating(HWND taskbar) {
+    std::lock_guard lock(animationMutex);
     return animatingState.contains(taskbar);
 }
 
 void taskbar_animation::animate(HWND taskbar, const bool causedByMaximizedWindow) {
-    // int opacity = 0;
-    // GetLayeredWindowAttributes(taskbar, nullptr, reinterpret_cast<BYTE *>(&opacity), nullptr);
-    // opacity += hoveredOver ? config::animationOpacityStep : -config::animationOpacityStep;
-    // if (opacity <= config::opacityWhenHiddenInternal || opacity >= config::opacityWhenHoveredInternal)
-    //     return;
-
+    if (animatingState.contains(taskbar))
+        return;
+    const int visibleOpacityPoint = causedByMaximizedWindow ? config::opacityWhenShownInternal : config::opacityWhenHiddenInternal;
+    const int currentOpacity = nextOpacity(taskbar, visibleOpacityPoint);
+    SetLayeredWindowAttributes(taskbar, 0, static_cast<BYTE>(currentOpacity), LWA_ALPHA);
+    if (currentOpacity == visibleOpacityPoint || currentOpacity == config::opacityWhenHoveredInternal)
+        return;
     std::lock_guard lock(animationMutex);
-    if (!animatingState.contains(taskbar)) {
-        animatingState[taskbar] = causedByMaximizedWindow ? config::opacityWhenShownInternal : config::opacityWhenHiddenInternal;
-    }
+    animatingState[taskbar] = visibleOpacityPoint;
 }
--- taskbar_animation.h
+++ taskbar_animation.h
@@ -1,15 +1,14 @@
 #ifndef TASKBAR_ANIMATION_H
 #define TASKBAR_ANIMATION_H
 
-#include <unordered_map>
-#include <vector>
+#include <mutex>
 #include <windows.h>
 #include <bits/std_thread.h>
 
 class taskbar_animation {
 public:
-    static std::unordered_map<HWND, int> animatingState;
     static std::thread animationThread;
+    static std::mutex animationMutex;
 
     static void initThread();
     static bool isAnimating(HWND taskbar);
