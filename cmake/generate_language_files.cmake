efb3e9059e692d1b244ffdd9874fa66ef225f960
Tautvydas <tautvydas.gaming@gmail.com>
2026-09-20 19:47:04 +0300

    Update CMakeLists

--- cmake/generate_language_files.cmake
+++ cmake/generate_language_files.cmake
@@ -1,7 +1,7 @@
 # Use CMAKE_CURRENT_LIST_DIR to reference the source directory of the script.
 set(lang_id_counter 1)
 set(lang_h_output "")
-file(STRINGS "${CMAKE_CURRENT_LIST_DIR}/language.h.in" lines)
+file(STRINGS "${CMAKE_CURRENT_LIST_DIR}/../language.h.in" lines)
 foreach(line IN LISTS lines)
     string(FIND "${line}" "@LANG_ID_COUNTER@" pos)
     while(pos GREATER -1)
@@ -11,10 +11,10 @@ foreach(line IN LISTS lines)
     endwhile()
     set(lang_h_output "${lang_h_output}${line}\n")
 endforeach()
-file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/language.h" "${lang_h_output}")
+file(WRITE "${CMAKE_CURRENT_BINARY_DIR_CONFIGURED}/language.h" "${lang_h_output}")
 
 # Read the generated language.h from the binary directory for mapping.
-file(STRINGS "${CMAKE_CURRENT_BINARY_DIR}/language.h" gen_lines)
+file(STRINGS "${CMAKE_CURRENT_BINARY_DIR_CONFIGURED}/language.h" gen_lines)
 set(LANGUAGE_MAPPING_ENTRIES "")
 foreach(line IN LISTS gen_lines)
     if(line MATCHES "^#define[ \t]+(MSG_[A-Z0-9_]+)[ \t]+[0-9]+")
@@ -23,4 +23,5 @@ foreach(line IN LISTS gen_lines)
     endif()
 endforeach()
 
-configure_file("${CMAKE_CURRENT_LIST_DIR}/language.cpp.in" "${CMAKE_CURRENT_BINARY_DIR}/language.cpp" @ONLY)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/../language.cpp.in" "${CMAKE_CURRENT_BINARY_DIR_CONFIGURED}/language.cpp" @ONLY)
+
